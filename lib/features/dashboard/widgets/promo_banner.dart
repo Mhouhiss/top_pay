12ab522b957d3294ef369da4da9efb061692f2cb
Muizdeen Adeyemi <muizdeenadeyemi@gmail.com>
2026-09-21 23:57:38 +0100

    feat: implement dashboard screen

---
+++ lib/features/dashboard/widgets/promo_banner.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PromoBanner extends StatelessWidget {
+  const PromoBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Promo Banner');
+  }
+}
